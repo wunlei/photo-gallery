2d71903992153b60a1476b6b20ed60b8321527bc
wunlei <confusioncosmos@gmail.com>
2022-03-23 15:31:54 +0300

    feat: add router tests

---
+++ react-components/src/components/AppRouter/AppRouter.test.tsx
@@ -0,0 +1,27 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import AppRouter from './AppRouter';
+
+test('app rendering/navigating', () => {
+  render(<AppRouter></AppRouter>);
+
+  let searchInput = screen.getByPlaceholderText(/search/i);
+  expect(searchInput).toBeInTheDocument();
+
+  const leftClick = { button: 0 };
+
+  userEvent.click(screen.getByText(/about us/i), leftClick);
+  const pageAboutUs = document.querySelector('.page-about');
+  expect(pageAboutUs).toBeInTheDocument();
+
+  userEvent.click(screen.getByText(/main/i), leftClick);
+  searchInput = screen.getByPlaceholderText(/search/i);
+  expect(searchInput).toBeInTheDocument();
+});
+
+test('load non-existing rout', () => {
+  window.history.replaceState({}, '', '/random-route');
+  render(<AppRouter></AppRouter>);
+  expect(screen.getByText(/Page Not Found/i)).toBeInTheDocument();
+});
