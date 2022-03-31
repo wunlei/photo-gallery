330c6e7073d8f4f2881e2d01f3649d38d42e0b26
wunlei <confusioncosmos@gmail.com>
2022-03-31 20:54:00 +0300

    feat: add route for contribute page

---
+++ react-components/src/views/Contribute/Contribute.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Contribute extends React.Component {
+  render() {
+    return (
+      <main className="page page-contribute">
+        <h3 className="page__title">Contribute</h3>
+      </main>
+    );
+  }
+}
+
+export default Contribute;
