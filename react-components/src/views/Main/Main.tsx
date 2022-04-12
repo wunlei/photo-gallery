9c1971e746b3926ed049198a88f9d45e4f4e8eb9
wunlei <confusioncosmos@gmail.com>
2022-04-12 15:04:16 +0300

    feat: add hooks to main page

--- react-components/src/views/Main/Main.tsx
+++ react-components/src/views/Main/Main.tsx
@@ -1,54 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from 'components/Card/Card';
 import Loader from 'components/Loader/Loader';
 import SearchBar from 'components/SearchBar/SearchBar';
 import { getSearchResults } from 'api/Api';
-import { PropsMainPage, StateMainPage } from './Main.types';
+import { ApiSearchData } from 'api/Api.types';
 import './Main.scss';
 
-class MainPage extends React.Component<PropsMainPage, StateMainPage> {
-  query = React.createRef<HTMLInputElement>();
-  constructor(props: PropsMainPage) {
-    super(props);
-    this.state = {
-      data: [],
-      isLoading: false,
-    };
-    this.handleInputChange = this.handleInputChange.bind(this);
-  }
+function MainPage() {
+  const query = React.createRef<HTMLInputElement>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [data, setData] = useState<ApiSearchData[]>([]);
 
-  async handleInputChange() {
-    if (this.query.current) {
-      const value = this.query.current.value.trim();
+  async function handleInputChange() {
+    if (query.current) {
+      const value = query.current.value.trim();
       if (value) {
-        this.setState({ isLoading: true });
-        const data = await getSearchResults(this.query.current.value);
-        this.setState({ isLoading: false });
-        this.setState({ data: data.results });
+        setIsLoading(true);
+
+        console.log(query.current.value);
+        const data = await getSearchResults(query.current.value);
+
+        setIsLoading(false);
+        setData(data.results);
       }
     }
   }
 
-  render() {
-    return (
-      <main className="page page-main">
-        <h3 className="page__title">Search for images</h3>
-        <SearchBar handleInputChange={this.handleInputChange} reference={this.query} />
-        <div className="cards-container">
-          {this.state.isLoading ? <Loader /> : null}
-          {this.state.data.map((data) => (
-            <Card
-              key={data.id}
-              imgUrl={data.urls.regular}
-              author={data.user.name}
-              id={data.id}
-              description={data.alt_description}
-            />
-          ))}
-        </div>
-      </main>
-    );
-  }
+  return (
+    <main className="page page-main">
+      <h3 className="page__title">Search for images</h3>
+      <SearchBar
+        handleInputChange={() => {
+          handleInputChange();
+        }}
+        reference={query}
+      />
+      <div className="cards-container">
+        {isLoading ? <Loader /> : null}
+        {data.map((data) => (
+          <Card
+            key={data.id}
+            imgUrl={data.urls.regular}
+            author={data.user.name}
+            id={data.id}
+            description={data.alt_description}
+          />
+        ))}
+      </div>
+    </main>
+  );
 }
 
 export default MainPage;
