9dfb3529d33bda7a6ae2f1c5778bbf29910ff20d
wunlei <confusioncosmos@gmail.com>
2022-04-16 19:42:07 +0300

    fix: update form tests

--- react-components/src/components/FormContribute/FormContribute.test.tsx
+++ react-components/src/components/FormContribute/FormContribute.test.tsx
@@ -1,10 +1,10 @@
 import '@testing-library/jest-dom';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Contribute from 'views/Contribute/Contribute';
 import countriesList from './CountriesList';
 
-test('Should render error messages for required inputs', () => {
+test('Should render error messages for required inputs', async () => {
   Object.defineProperty(URL, 'createObjectURL', {
     writable: true,
     value: jest.fn(),
@@ -16,8 +16,9 @@ test('Should render error messages for required inputs', () => {
   const nameInput = screen.getByLabelText(/name/i);
   userEvent.type(nameInput, 'John');
   userEvent.click(submitBtn);
-  const errorsFieldsCount = document.querySelectorAll('.form-error-message').length;
-  expect(screen.getAllByText(/required/i).length).toEqual(errorsFieldsCount);
+  await waitFor(() => {
+    expect(screen.getAllByText(/required/i)).toHaveLength(5);
+  });
 });
 
 test('Should render card after successful submit', async () => {
@@ -29,8 +30,20 @@ test('Should render card after successful submit', async () => {
   userEvent.selectOptions(screen.getByRole('combobox'), countriesList[0]);
 
   const fileInput = screen.getByLabelText(/upload/i) as HTMLInputElement;
-  const fakeFile = new File(['hello'], 'hello.png', { type: 'image/png' });
-  userEvent.upload(fileInput, fakeFile);
+  const fakeFile = new File(['hello'], 'hello.jpeg', { type: 'image/jpeg' });
+  await waitFor(() => userEvent.upload(fileInput, fakeFile));
+
+  await waitFor(() => {
+    userEvent.upload(fileInput, fakeFile);
+  });
+
+  Object.defineProperty(fileInput, 'files', { value: [fakeFile] });
+  Object.defineProperty(fileInput, 'value', {
+    value: fakeFile.name,
+  });
+  fireEvent.change(fileInput);
+
+  fireEvent.click(screen.getByText(/submit/i));
 
   const dateInput = screen.getByLabelText(/date/i) as HTMLInputElement;
   fireEvent.change(dateInput, { target: { value: '2020-01-01' } });
@@ -43,8 +56,10 @@ test('Should render card after successful submit', async () => {
 
   const submitBtn = screen.getByText(/submit/i);
   userEvent.click(submitBtn);
-  const submitBtnSuccess = screen.getByText(/Successfully/i);
-  expect(submitBtnSuccess).toBeInTheDocument();
+
+  await waitFor(() => {
+    expect(screen.getByText(/Successfully/i)).toBeInTheDocument();
+  });
 
   const card = document.querySelector('.card-contribute');
   expect(card).toBeInTheDocument();
